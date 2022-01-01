23274287db6d419e2123b72c08627f7de15212d3
treatme030 <treatme8192@gmail.com>
2022-01-01 19:53:54 +0900

    feat : 회원가입 기능구현

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,14 +1,18 @@
 import React, { useCallback, useState } from 'react';
+import axios from 'axios';
 import useInput from '@hooks/useInput';
 import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from '@pages/SignUp/styles';
 
 const SignUp = () => {
 
-  const [nickname, onChangeEmail, setNickname] = useInput('');
-  const [email, onChangeNickname, setEmail] = useInput('');
+  const [nickname, onChangeNickname, setNickname] = useInput('');
+  const [email, onChangeEmail, setEmail] = useInput('');
   const [password, setpassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
   const [mismatchError, setMismatchError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [signUpError, setSignUpError] = useState('');
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
 
   const onChangePassword = useCallback((e) => {
     setpassword(e.target.value);
@@ -22,8 +26,28 @@ const SignUp = () => {
 
   const onSubmit = useCallback((e) => {
     e.preventDefault();
-    if(!mismatchError){
-      console.log('')
+    if([nickname, email, password].includes('')){
+      setErrorMessage(true);
+      return;
+    } else if(mismatchError){
+      return;
+    } else {
+      setSignUpSuccess(false);
+      setSignUpError('');
+      axios.post('http://localhost:3095/api/users', {
+        email, 
+        nickname, 
+        password
+      })
+      .then((response) => {
+        console.log(response);
+        setSignUpSuccess(true);
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setSignUpError(error.response.data);
+      })
+      .finally(() => {});
     }
   },[email, nickname, password, passwordCheck, mismatchError]);
 
@@ -34,19 +58,37 @@ const SignUp = () => {
         <Label id="email-label">
           <span>이메일 주소</span>
           <div>
-            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+            <Input 
+            type="email" 
+            id="email" 
+            name="email" 
+            value={email} 
+            onChange={onChangeEmail} 
+            />
           </div>
         </Label>
         <Label id="nickname-label">
           <span>닉네임</span>
           <div>
-            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+            <Input 
+            type="text" 
+            id="nickname" 
+            name="nickname" 
+            value={nickname} 
+            onChange={onChangeNickname} 
+            />
           </div>
         </Label>
         <Label id="password-label">
           <span>비밀번호</span>
           <div>
-            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+            <Input 
+            type="password" 
+            id="password" 
+            name="password" 
+            value={password} 
+            onChange={onChangePassword} 
+            />
           </div>
         </Label>
         <Label id="password-check-label">
@@ -61,9 +103,9 @@ const SignUp = () => {
             />
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
-          {/* {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {signUpError && <Error>이미 가입된 이메일입니다.</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {errorMessage && <Error>빈 칸을 입력해주세요.</Error>}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
