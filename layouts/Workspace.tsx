373c647f7384449f2b6e49874ebe4eb4947cf0ce
treatme030 <treatme8192@gmail.com>
2022-01-02 11:47:36 +0900

    feat : workspace 만들고, 로그아웃 구현

---
+++ layouts/Workspace.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useCallback } from 'react';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
+const Workspace: FC = ({ children }) => {
+  const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
+
+  const onLogout = useCallback(() => {
+    axios.post('http://localhost:3095/api/users/logout', null, {
+      withCredentials: true,
+    })
+    .then((response) => {
+      mutate();
+    })
+    .catch((error) => {})
+  },[]);
+
+  if(!data){
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <div>
+      <button onClick={onLogout}>로그아웃</button>
+      { children }
+    </div>
+  );
+};
+
+export default Workspace;
---
+++ pages/Channel/index.tsx
@@ -0,0 +1,14 @@
+import Workspace from '@layouts/Workspace';
+import React from 'react';
+
+const Channel = () => {
+  return (
+    <Workspace>
+      <div>
+        channel
+      </div>
+    </Workspace>
+  );
+};
+
+export default Channel;
