d4df70326695c408aee38103fc8f1da26462b182
treatme030 <treatme8192@gmail.com>
2022-01-03 10:21:42 +0900

    feat : 워크스페이스에 멤버 초대 모달 생성

---
+++ components/InviteWorkspaceModal/index.tsx
@@ -0,0 +1,66 @@
+import React, { useCallback, useState, VFC } from 'react';
+import Modal from '@components/Modal';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
+import { IUser } from '@typings/db';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface Props {
+  setShow: (flag: boolean) => void;
+  onCloseModal: () => void;
+}
+
+const InviteWorkspaceModal: VFC<Props> = ({ onCloseModal, setShow }) => {
+  const { workspace } = useParams<{ workspace: string }>();
+  const { data: userData } = useSWR<IUser>('http://localhost:3095/api/users', fetcher);
+  const { mutate } = useSWR<IUser[]>(userData ? 
+    `http://localhost:3095/api/workspaces/${workspace}/members` : null, fetcher);
+
+  const [newMember, onChangeNewMember, setNewMember] = useInput('')
+
+  const onInviteMember = useCallback((e) => {
+    e.preventDefault();
+    if(!newMember || !newMember.trim()){
+      return;
+    }
+    toast.configure();
+    axios.post(`http://localhost:3095/api/workspaces/${workspace}/members`, {
+      email: newMember,
+    }, {
+      withCredentials: true,
+    })
+    .then((response) => {
+      mutate();
+      setShow(false);
+      setNewMember('');
+    })
+    .catch((error) => {
+      console.dir(error);
+      toast.error(error.response?.data, { position: 'bottom-center' });
+    })
+  },[newMember, workspace, setShow, setNewMember, mutate]);
+
+  return (
+    <Modal onCloseModal={onCloseModal} >
+      <form onSubmit={onInviteMember}>
+        <Label id="member">
+          <span>Email</span>
+          <Input 
+          id="member" 
+          type="email" 
+          value={newMember} 
+          onChange={onChangeNewMember} 
+          />
+        </Label>
+        <Button type="submit">초대하기</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default InviteWorkspaceModal;
